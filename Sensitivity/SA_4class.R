ab6e91de3a0ef7dfd79e46d0ce5157b34d2ec572
wmt2024 <wmt@tju.edu.cn>
2025-11-29 18:55:40 +0800

    Add files via upload

---
+++ Sensitivity/SA_4class.R
@@ -0,0 +1,56 @@
+library(haven)
+library(dplyr)
+library(tidyr)
+library(car)
+library(broom)
+
+df <- read_sav("R_ana.sav") %>%
+  mutate(
+    SA      = as.factor(SA),
+    gender  = as.factor(gender),
+    grade   = as.factor(grade),
+    profile = as.factor(profile),
+    age     = as.numeric(age),
+    BDI     = as.numeric(BDI),
+    SAI     = as.numeric(SAI)
+  ) %>%
+  drop_na(SA, age, grade, gender, BDI, SAI, profile)
+
+df$profile <- relevel(df$profile, ref = "4")
+
+model1 <- glm(SA ~ age + grade + gender, data = df, family = binomial)
+model2 <- glm(SA ~ age + grade + gender + BDI + SAI, data = df, family = binomial)
+model3 <- glm(SA ~ age + grade + gender + BDI + SAI + profile, data = df, family = binomial)
+
+extract_results <- function(model, adjust_FDR = FALSE) {
+  res <- tidy(model) %>%
+    mutate(
+      OR = exp(estimate),
+      lower_CI = exp(estimate - 1.96 * std.error),
+      upper_CI = exp(estimate + 1.96 * std.error)
+    ) %>%
+    select(term, estimate, std.error, OR, lower_CI, upper_CI, p.value)
+  
+  if (adjust_FDR) {
+    res <- res %>%
+      mutate(p_FDR = p.adjust(p.value, method = "BH"))
+  }
+  
+  return(res)
+}
+
+cat("\nMODEL 1 RESULTS\n")
+print(extract_results(model1))
+
+cat("\nMODEL 2 RESULTS\n")
+print(extract_results(model2))
+
+cat("\nMODEL 3 RESULTS (FINAL MODEL) WITH BH-FDR\n")
+print(extract_results(model3, adjust_FDR = TRUE))
+
+cat("\nMODEL COMPARISON (Δχ²)\n")
+anova_out <- anova(model1, model2, model3, test = "Chisq")
+print(anova_out)
+
+cat("\nVIF VALUES\n")
+print(vif(model3))
---
+++ Sensitivity/SA_TAI.R
@@ -0,0 +1,57 @@
+library(haven)
+library(dplyr)
+library(tidyr)
+library(car)
+library(broom)
+
+df <- read_sav("R_ana.sav") %>% 
+  mutate(
+    SA     = as.factor(SA),
+    gender = as.factor(gender),
+    grade  = as.factor(grade),
+    class  = as.factor(class),
+    age    = as.numeric(age),
+    BDI    = as.numeric(BDI),
+    TAI    = as.numeric(TAI)       
+  ) %>%
+  drop_na(SA, age, grade, gender, BDI, TAI, class)  
+
+model1 <- glm(SA ~ age + grade + gender, data = df, family = binomial)
+
+model2 <- glm(SA ~ age + grade + gender + BDI + TAI,  
+              data = df, family = binomial)
+
+model3 <- glm(SA ~ age + grade + gender + BDI + TAI + class,  
+              data = df, family = binomial)
+
+extract_results <- function(model) {
+  tidy(model) %>%
+    mutate(
+      OR = exp(estimate),
+      lower_CI = exp(estimate - 1.96 * std.error),
+      upper_CI = exp(estimate + 1.96 * std.error)
+    ) %>%
+    select(term, estimate, std.error, OR, lower_CI, upper_CI, p.value)
+}
+
+cat("\nMODEL 1 RESULTS\n")
+print(extract_results(model1))
+
+cat("\nMODEL 2 RESULTS\n")
+print(extract_results(model2))
+
+cat("\nMODEL 3 RESULTS\n")
+results_model3 <- extract_results(model3)
+print(results_model3)
+
+cat("\nMODEL 3 BH-FDR Corrected p-values\n")
+results_model3 <- results_model3 %>%
+  mutate(p_FDR_BH = p.adjust(p.value, method = "BH"))
+print(results_model3)
+
+cat("\nHierarchical Model Comparison (Δχ²)\n")
+anova_out <- anova(model1, model2, model3, test = "Chisq")
+print(anova_out)
+
+cat("\nVIF Values (Model 3)\n")
+print(vif(model3))
